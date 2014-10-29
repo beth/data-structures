3bf636569f265beb26d6c1b68f37f9cb7e331292
bethjohnson <bethjohnson3@gmail.com>
2014-10-29 14:55:52 -0700

    finshed stack for functional

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,4 +1,5 @@
 var makeStack = function(){
+  var count = 0;
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -6,12 +7,22 @@ var makeStack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+    count ++;
+    storage[count] = value;
   };
 
   someInstance.pop = function(){
+    if(count > 0){
+      var value = storage[count];
+      count --;
+      return value;
+    } else{
+      return null;
+    }
   };
 
   someInstance.size = function(){
+    return count;
   };
 
   return someInstance;
