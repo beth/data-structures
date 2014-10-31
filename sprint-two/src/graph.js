3132326633eece084c419df26d622d13dab73063
Hack Reactor Students <accounts@hackreactor.com>
2014-10-31 16:41:33 -0700

    In progress attempt at graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,22 +1,39 @@
 var Graph = function(){
+  this.nodeList = {};
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+  if(Object.keys(this.nodeList).length === 1){
+    toNode = Object.keys(this.nodeList)[0];
+
+  }
+
+  var newNodeObj = new Node(newNode);
+  //newNode.connections.push();
+  this.nodeList[newNode] = newNodeObj;
+
 };
 
 Graph.prototype.contains = function(node){
+  return node in this.nodeList;
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.nodeList[node];
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+};
+
+var Node = function(value){
+ this.connections = [];
 };
 
 /*
