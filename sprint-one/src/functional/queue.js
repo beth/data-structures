41d50648249f8acfad0f70862b8841496edd47fa
BethJohnson <BethJohnson3@gmail.com>
2015-01-23 15:43:29 -0800

    Implements stack and queue for functional style

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,24 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var start = 0;
+  var end = -1;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[++end] = value;
   };
 
   someInstance.dequeue = function(){
+    if(start<=end){
+      return storage[start++];
+    }else
+      return null;
   };
 
   someInstance.size = function(){
+    return end - start + 1;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,22 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var start = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[start++] = value;
   };
 
   someInstance.pop = function(){
+    if(start > 0)
+      return storage[--start];
+    else
+      return null;
   };
 
   someInstance.size = function(){
+    return start;
   };
 
   return someInstance;
