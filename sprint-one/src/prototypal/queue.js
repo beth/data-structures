8778d5367992f94f069129b16d6b0edccc2a05be
BethJohnson <BethJohnson3@gmail.com>
2015-01-25 07:18:30 -0800

    Implements stack and queue for protoypal style

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,26 @@
 var makeQueue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.storage = {};
+  obj.start = 0;
+  obj.end = -1;
+  return obj;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value){
+  this.storage[++this.end] = value;
+};
+
+queueMethods.dequeue = function(){
+  if(this.end>=this.start){
+    return this.storage[this.start++];
+  }else
+    return null;
+};
+
+queueMethods.size = function(){
+  return this.end - this.start + 1;
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,24 @@
 var makeStack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.storage = {};
+  obj.start = 0;
+  return obj;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+  this.storage[this.start++] = value;
+};
+
+stackMethods.pop = function(){
+  if(this.start>0){
+    return this.storage[--this.start];
+  }else
+    return null;
+};
+
+stackMethods.size = function(){
+  return this.start;
+};
 
