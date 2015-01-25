909b21c7a23c61d70006a85a575806debc2ecb0a
BethJohnson <BethJohnson3@gmail.com>
2015-01-25 07:14:48 -0800

    Implements stack and queue for functional-shared style

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,29 @@
 var makeQueue = function(){
+  var obj = {};
+  obj.storage = {};
+  obj.start = 0;
+  obj.end = -1;
+  _.extend(obj,queueMethods);
+
+  return obj;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var queueMethods = {};
+queueMethods = {};
+queueMethods.enqueue = function(value){
+  this.storage[++this.end] = value;
+};
 
+queueMethods.dequeue = function(){
+  if(this.end>=this.start){
+    return this.storage[this.start++];
+  }else
+    return null;
+};
+
+queueMethods.size = function(){
+  return this.end - this.start + 1;
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,25 @@
 var makeStack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = {}
+  obj.storage = {};
+  obj.end = 0;
+  _.extend(obj, stackMethods);
+  return obj;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+  this.storage[this.end++] = value;
+};
+
+stackMethods.pop = function(){
+  if(this.end>0){
+    return this.storage[--this.end];
+  }else
+    return null;
+}
+
+stackMethods.size = function(){
+  return this.end;
+}
 
