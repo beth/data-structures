582d1f8c3e3271072d71b5572f3cf50860f958a1
BethJohnson <BethJohnson3@gmail.com>
2015-01-25 07:24:54 -0800

    Implements stack and queue for pseduclassical style

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,21 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.start = 0;
 };
+
+Stack.prototype.push = function(value){
+  this.storage[this.start++] = value;
+}
+
+Stack.prototype.pop = function(){
+  if(this.start>0){
+    return this.storage[--this.start];
+  }else
+    return null;
+}
+
+Stack.prototype.size = function(){
+  return this.start;
+}
 
 
