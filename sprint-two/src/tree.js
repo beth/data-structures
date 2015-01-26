56869ac65f124e631bb6e7bea9677b5241885d0d
BethJohnson <BethJohnson3@gmail.com>
2015-01-26 13:44:16 -0800

    Implements tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,33 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  _.extend(newTree,treeMethods);
   return newTree;
 };
-
-
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
+  var found = false;
 
+  var recursiveSearch = function(node){
+    if(node.value === target)
+    {
+      found = true;
+    }else{
+      for(var i = 0; i<node.children.length; i++){
+        recursiveSearch(node.children[i]);
+      }
+    }
+  };
+
+  recursiveSearch(this);
+  return found;
 };
 
 
