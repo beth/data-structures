f6d5b8483f775fac9a58c9308dc44090990df19f
BethJohnson <BethJohnson3@gmail.com>
2015-01-26 05:24:57 -0800

    Implements linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+    if(this.head === null){
+      this.head = this.tail = newNode;
+    }else{
+      this.tail.next = this.tail = newNode; 
+    }
+
   };
 
   list.removeHead = function(){
+    if(this.head != null){
+      var node = this.head;
+      this.head = this.head.next;
+      return node.value;
+    }else
+      return null;
   };
 
   list.contains = function(target){
+    var node = this.head;
+    while(node != null){
+      if(node.value === target){
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
   };
 
   return list;
