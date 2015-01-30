bd51f585e1381545151a19189ef08f6404ce40c3
BethJohnson <BethJohnson3@gmail.com>
2015-01-30 09:03:40 -0600

    Implements hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,17 +3,51 @@ var HashTable = function(){
   this._storage = makeLimitedArray(this._limit);
 };
 
+var getBucket = function(k,limit){
+  var i = getIndexBelowMaxForKey(k,limit);
+  return this._storage.get(i);
+}
+
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i) || [];
+  var found = false;
+  for(var j = 0; j<bucket.length; j++){
+    if(bucket[j][0] === k){
+      bucket[j][1] = v;
+      found = true;
+      break;
+    }
+  }
+  if(!found){
+    bucket.push([k,v]);
+    this._storage.set(i,bucket);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage.get(i);
+  if(bucket){
+    for(var j = 0; j<bucket.length; j++){
+      if(bucket[j][0] === k){
+        return bucket[j][1];
+      }
+    }
+  }else
+    return null;
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  if(bucket){
+    for(var j = 0; j<bucket.length; j++){
+      if(bucket[j][0] === k){
+        bucket[j][1] = null;
+      }
+    }
+  }
 };
 
 
