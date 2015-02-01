227e0f75dd5ae1f5442f81db479f809cd43298cd
BethJohnson <BethJohnson3@gmail.com>
2015-02-01 11:21:56 -0600

    Refactors set to allow input objects of any type

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,17 +7,23 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
-  this._storage[item] = true;
+  this._storage[myStringify(item)] = true;
 };
 
 setPrototype.contains = function(item){
-  return this._storage.hasOwnProperty(item);
+  return this._storage.hasOwnProperty(this._myStringify(item));
 };
 
 setPrototype.remove = function(item){
-  delete this._storage[item];
+  delete this._storage[this._myStringify(item)];
 };
 
+setPrototype._myStringify = function(item){
+  if(typeof item !== "string"){
+    return JSON.stringify(item);
+  }else
+    return item;
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
